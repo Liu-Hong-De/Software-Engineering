81a845a4260179c87f04882d91a29584f074acdd
MI <MI@DESKTOP-FLAJOTJ>
2019-12-25 00:31:01 +0800

    add main in Grade

--- grade/src/grade/Grade.java
+++ grade/src/grade/Grade.java
@@ -25,4 +25,19 @@ public class Grade {
 		}
 		return grade;
 	}
+	
+	public static void main(String[] args) { 
+		System.out.print("Enetr the score = ");  
+		try {   
+			InputStreamReader isr = new InputStreamReader(System.in);   
+			BufferedReader br = new BufferedReader(isr);   
+			int score = Integer.parseInt(br.readLine());       
+			char grade = letterGrade(score);       
+			System.out.println("The grade of " + score + " is " + grade);
+		} catch (NumberFormatException ex) {  
+			System.out.println("Not an integer!");  
+		} catch (IOException e) {   
+			e.printStackTrace();  
+		}        
+	} 	 
 }
